18cb99975fe24d703b906d27abe047e9df032a1d
Suraj Chaudhary <isurajcdry@gmail.com>
2025-10-29 06:48:29 -0400

    Armstrong and Prime

---
+++ src/Armstrong.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("\nEnter the number to check armstrong: ");
+        int num = kb.nextInt();
+
+        if (isArmstrong(num)) {
+            System.out.println("Armstrong.\n");
+        } else {
+            System.out.println("Not armstrong.\n");
+        }
+
+        kb.close();
+    }
+
+    static boolean isArmstrong(int num) {
+        int temp, sum = 0, original = num;
+        int power = String.valueOf(original).length(); // for any number of digits
+
+        while (num > 0) {
+            temp = num % 10;
+            sum = sum + (int) (Math.pow(temp, power));
+            num = num / 10;
+        }
+
+        return (sum == original);
+    }
+}
---
+++ src/Prime.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = kb.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println("Prime");
+        } else if (num <= 1) {
+            System.out.println("Neither prime nor composite.");
+        } else {
+            System.out.println("Composite");
+        }
+        kb.close();
+    }
+
+    static boolean isPrime(int num) {
+        if (num == 2) {
+            return true;
+        } else if (num <= 1 || num % 2 == 0) {
+            return false;
+        } else {
+            int limit = (int) Math.sqrt(num);
+            for (int i = 3; i <= limit; i += 2) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
