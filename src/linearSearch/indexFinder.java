ad04840b92a5c0657951d01eb8a024ddbe9097a9
Suraj Chaudhary <isurajcdry@gmail.com>
2025-11-03 16:51:07 -0500

    linear search index finder added

---
+++ src/linearSearch/indexFinder.java
@@ -0,0 +1,43 @@
+package linearSearch;
+
+import java.util.Scanner;
+
+public class indexFinder {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("\nLength of array: ");
+        int size = kb.nextInt();
+
+        int[] arr = new int[size];
+        System.out.print("Enter an array (separated by space): ");
+        for (int i = 0; i < size; i++){
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.print("Target: ");
+        int target = kb.nextInt();
+
+        int index = findTheIndex(arr, target);
+
+        if (index == -1){
+            System.out.println("\nTarget not found.\n");
+        } else {
+            System.out.println("\nThe index: " + index + "\n");
+        }
+    }
+
+    static int findTheIndex(int[] arr, int target){
+        if (arr.length == 0){
+            return -1;
+        }
+
+        for (int index = 0; index < arr.length; index++){
+            if (arr[index] == target){
+                return index;
+            }
+        }
+
+        return -1;
+    }
+}
